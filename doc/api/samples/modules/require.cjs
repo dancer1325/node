a581fc6b9ae633cadb55eed16c5f950a0b9dd2c6
alfredo.toledano <alfredo.toledano@allianzdirect.es>
2025-01-03 14:06:39 +0100

    doc(doc.api.modules): add notes and samples

---
+++ doc/api/samples/modules/require.cjs
@@ -0,0 +1,12 @@
+const distance = require('./distance.mjs');
+console.log(distance);
+// [Module: null prototype] {
+//   distance: [Function: distance]
+// }
+
+const point = require('./point.mjs');
+console.log(point);
+// [Module: null prototype] {
+//   default: [class Point],
+//   __esModule: true,
+// }
